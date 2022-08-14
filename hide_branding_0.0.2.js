d3c5550f00ed7a3110c6d4aa90398e2aaf7b923f
Kaung Khant Kyaw <58139120+kaungkhantjc@users.noreply.github.com>
2022-08-14 07:41:01 +0630

    hide "Powered by 000webhost"

---
+++ hide_branding_0.0.2.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.querySelector('body > div:last-of-type').remove();
+    document.querySelector('body > script:last-of-type').remove();
+});
