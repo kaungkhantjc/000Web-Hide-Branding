92bf94e98fe81c3e82029bbde4ebad9fdc10c6a2
Kaung Khant Kyaw <58139120+kaungkhantjc@users.noreply.github.com>
2022-08-16 10:54:24 +0630

    Update hide_branding_0.0.2.js

--- hide_branding_0.0.2.js
+++ hide_branding_0.0.2.js
@@ -1,4 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.querySelector('body > div:last-of-type').remove();
-    document.querySelector('body > script:last-of-type').remove();
+document.addEventListener("DOMContentLoaded", function (event) {
+    let brandingDiv = document.querySelector('body > div:last-of-type');
+    if (brandingDiv.firstChild.title.includes("Hosted on free web hosting 000webhost.com")) {
+        brandingDiv.remove();
+    }
+
+    let brandingScript = document.querySelector('body > script:last-of-type');
+    if (brandingScript.innerText.includes("www.hostinger.com")) {
+        brandingScript.remove();
+    }
 });
