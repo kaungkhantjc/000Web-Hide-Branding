667369ac28cae357abe1a6fa50851db697b23979
Kaung Khant Kyaw <58139120+kaungkhantjc@users.noreply.github.com>
2022-08-16 11:48:29 +0630

    Update hide_branding_0.0.2.js

--- hide_branding_0.0.2.js
+++ hide_branding_0.0.2.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     let brandingDiv = document.querySelector('body > div:last-of-type');
-    if (brandingDiv.firstChild.title.includes("Hosted on free web hosting 000webhost.com")) {
+    if (brandingDiv !== null && brandingDiv.firstChild !== null && brandingDiv.firstChild.title !== undefined && brandingDiv.firstChild.title.includes("Hosted on free web hosting 000webhost.com")) {
         brandingDiv.remove();
     }
 
     let brandingScript = document.querySelector('body > script:last-of-type');
-    if (brandingScript.innerText.includes("www.hostinger.com")) {
+    if (brandingScript !== null && brandingScript.innerText.includes("www.hostinger.com")) {
         brandingScript.remove();
     }
 });
